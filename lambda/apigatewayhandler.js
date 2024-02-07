4e37336a19ea84bbf88e1ed7901ea0139a55ab07
bala <vbalakumar.cse@gmail.com>
2024-02-07 00:34:37 -0500

    added sqs

--- lambda/apigatewayhandler.js
+++ lambda/apigatewayhandler.js
@@ -10,61 +10,72 @@ const tableName = process.env.table;
 exports.handler = async function (event, context) {
   let body;
   let statusCode = 200;
-  console.log("tablename",tableName)
+  console.log("tablename", tableName);
   const headers = {
     "Content-Type": "application/json",
   };
   try {
     console.log(event);
     console.log("Event Route Key: ", event.routeKey);
-    switch (event.routeKey) {
-      case "DELETE /items/{id}":
-        await dynamo.send(
-          new DeleteCommand({
-            TableName: tableName,
-            Key: {
-              id: event.pathParameters.id,
-            },
-          })
-        );
-        body = `Deleted item ${event.pathParameters.id}`;
-        break;
-      case "GET /itemsbytype/{id}":
-        body = await dynamo.send(new ScanCommand({ TableName: tableName, FilterExpression: "contains(#columnname, :value)", ExpressionAttributeNames: { "#columnname": "type" }, ExpressionAttributeValues: { ":value": event.pathParameters.id } }));
-        body = body.Items;
-        break;
-      case "GET /items/{id}":
-        body = await dynamo.send(new ScanCommand({ TableName: tableName, FilterExpression: "contains(#columnname, :value)", ExpressionAttributeNames: { "#columnname": "slug" }, ExpressionAttributeValues: { ":value": event.pathParameters.id } }));
-        body = body.Items;
-        break;
-      case "PUT /items":
-        // from SQS
-        const { Records } = event;
-        const requestJSON = JSON.parse(Records[0].body);
-        //let requestJSON = JSON.parse(event.body);
-        await dynamo.send(
-          new PutCommand({
-            TableName: tableName,
-            Item: requestJSON,
-          })
-        );
-        body = `Put item ${requestJSON.id}`;
-        break;
-      case "POST /items":
-        // from SQS
-        const { Records1 } = event;
-        const requestJSON1 = JSON.parse(Records1[0].body);
-        //let requestJSON = JSON.parse(event.body);
-        await dynamo.send(
-          new UpdateCommand({
-            TableName: tableName,
-            Item: requestJSON1,
-          })
-        );
-        body = `Post item ${requestJSON1.id}`;
-        break;
-      default:
-        throw new Error(`Unsupported route: "${event.routeKey}"`);
+    if (event?.Records[0]?.eventSource === "aws:sqs") {
+      console.log("Incoming message body from SQS : ", event);
+      const { Records } = event;
+      const requestJSON = JSON.parse(Records[0].body);
+      await dynamo.send(
+        new PutCommand({
+          TableName: tableName,
+          Item: requestJSON,
+        })
+      );
+      console.log("SQS request Successfully written to DynamoDB");
+    } else {
+      switch (event.routeKey) {
+        case "DELETE /items/{id}":
+          await dynamo.send(
+            new DeleteCommand({
+              TableName: tableName,
+              Key: {
+                id: event.pathParameters.id,
+              },
+            })
+          );
+          body = `Deleted item ${event.pathParameters.id}`;
+          break;
+        case "GET /itemsbytype/{id}":
+          body = await dynamo.send(new ScanCommand({ TableName: tableName, FilterExpression: "contains(#columnname, :value)", ExpressionAttributeNames: { "#columnname": "type" }, ExpressionAttributeValues: { ":value": event.pathParameters.id } }));
+          body = body.Items;
+          break;
+        case "GET /items/{id}":
+          body = await dynamo.send(new ScanCommand({ TableName: tableName, FilterExpression: "contains(#columnname, :value)", ExpressionAttributeNames: { "#columnname": "slug" }, ExpressionAttributeValues: { ":value": event.pathParameters.id } }));
+          body = body.Items;
+          break;
+        // case "PUT /items":
+        //
+        //   const { Records } = event;
+        //   const requestJSON = JSON.parse(Records[0].body);
+        //   await dynamo.send(
+        //     new PutCommand({
+        //       TableName: tableName,
+        //       Item: requestJSON,
+        //     })
+        //   );
+        //   body = `Put item ${requestJSON.id}`;
+        //   break;
+        // case "POST /items":
+        //   // from SQS
+        //   const { Records1 } = event;
+        //   const requestJSON1 = JSON.parse(Records1[0].body);
+        //   await dynamo.send(
+        //     new UpdateCommand({
+        //       TableName: tableName,
+        //       Item: requestJSON1,
+        //     })
+        //   );
+        //   body = `Post item ${requestJSON1.id}`;
+        //   break;
+        default:
+          throw new Error(`Unsupported route: "${event.routeKey}"`);
+      }
     }
   } catch (err) {
     statusCode = 400;
